25b22080c24dc3758dc756efddc253a211af40d9
Joyce Kao <kao5@purdue.edu>
2017-10-31 18:16:24 -0400

    separated time machine request and current forecast request in weather service

--- Personal Packing Assistant/WeatherService.swift
+++ Personal Packing Assistant/WeatherService.swift
@@ -21,10 +21,35 @@ class WeatherService {
         return (13.2476889,-57.2342027)
     }
     
-    public func weather(forDay: Day, inTrip: Trip, complete: @escaping (Error?, Weather?) -> Void) {
+    public func getHistorialWeather(forDay: Day, inTrip: Trip, complete: @escaping (Error?, Weather?) -> Void) {
         let l = latLong(forTrip: inTrip)
         
         forecastClient.timeMachine(latitude: l.0, longitude: l.1, date: forDay.date) { (forecast) in
+            if let e = forecast.error {
+                complete(e, nil)
+            }
+            
+            if let f = forecast.value, let report = f.daily?.first {
+                let w = Weather(
+                    weatherName: report.summary ?? "n/a",
+                    humidity: report.humidity ?? 0.0,
+                    precipitation: report.precipProbability ?? 0.0,
+                    wind: report.windSpeed ?? 0.0,
+                    maxTemp: report.temperatureMax ?? 0.0,
+                    minTemp: report.temperatureMin ?? 0.0,
+                    location: inTrip.destination,
+                    icon: report.icon ?? "n/a"
+                )
+                
+                complete(nil, w)
+            }
+        }
+    }
+    
+    public func getCurrentWeather(inTrip: Trip, complete: @escaping(Error?, Weather?) -> Void) {
+        let l = latLong(forTrip: inTrip)
+        
+        forecastClient.current(latitude: l.0, longitude: l.1) { (forecast) in
             if let e = forecast.error {
                 complete(e, nil)
             }
